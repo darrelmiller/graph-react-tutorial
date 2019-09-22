01765d68fb45a69fa5fe4651851ef771958dfcda
Darrel Miller <darrmi@microsoft.com>
2019-09-22 12:50:08 -0400

    updated with MSAL auth provider

---
+++ src/Config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    appId: 'a8de38f0-4a17-4c49-b138-90e594fe88be',
+    scopes: [
+      "user.read",
+      "calendars.read"
+    ]
+  };
---
+++ src/GraphService.js
@@ -0,0 +1,31 @@
+var graph = require('@microsoft/microsoft-graph-client');
+
+function getAuthenticatedClient(accessToken) {
+  // Initialize Graph client
+  const client = graph.Client.init({
+    // Use the provided access token to authenticate
+    // requests
+    authProvider: (done) => {
+      done(null, accessToken.accessToken);
+    }
+  });
+
+  return client;
+}
+
+export async function getUserDetails(client) {
+
+  const user = await client.api('/me').get();
+  return user;
+}
+
+export async function getEvents(client) {
+
+  const events = await client
+    .api('/me/events')
+    .select('subject,organizer,start,end')
+    .orderby('createdDateTime DESC')
+    .get();
+
+  return events;
+}
